212f930f0224113e64ebb472e9055d53152d9195
Carsten Buchberger <c.buchberger@witcom.de>
2019-05-22 14:21:37 +0200

    Prepared Staging-Process (#2)

--- src/test/java/de/witcom/api/WitcomApiGatewayApplicationTests.java
+++ src/test/java/de/witcom/api/WitcomApiGatewayApplicationTests.java
@@ -14,37 +14,17 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import de.witcom.api.model.OIDCCerts;
 import de.witcom.api.model.OIDCKey;
+import org.springframework.test.context.TestPropertySource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+//@TestPropertySource(locations="classpath:application.properties")
+//@ActiveProfiles("staging")
 public class WitcomApiGatewayApplicationTests {
-    
-    @Autowired
-    SessionRepository sessionRepo;
 
 	@Test
-	@Ignore
 	public void contextLoads() {
-	    Session session = new Session("my-app","fdsfdsfsdfds");
-	    sessionRepo.save(session);
-	    final Session retrievedSession = sessionRepo.findById(session.getSessionId()).get();
-        assertEquals(session.getSessionId(), retrievedSession.getSessionId());
 	}
 	
-	@Test
-	public void webclientTest() {
-	    
-	    RestTemplate restTemplate = new RestTemplate();
-	    OIDCCerts response3 = restTemplate.getForObject("https://auth.dev.witcom.services/auth/realms/witcom/protocol/openid-connect/certs", OIDCCerts.class);
-	    /*
-	   OIDCCerts response3 = WebClient.RequestBodySpec uri1 = WebClient.create("https://auth.dev.witcom.services2").method(HttpMethod.GET)
-            .uri("/auth/realms/witcom/protocol/openid-connect/certs");*/
-	   //OIDCCerts response3 = uri1.retrieve().bodyToMono(OIDCCerts.class).block(); 
-	   for (OIDCKey key:response3.keys){
-	       System.out.println(key.kid);
-	       
-	   }
-	   //System.out.println(response3.toString());
-	}
 
 }
