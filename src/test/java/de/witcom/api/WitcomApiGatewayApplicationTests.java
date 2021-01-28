fd0c09bc4ad00d4b5bfb21fd1e34ce7f47af0bda
Carsten Buchberger <c.buchberger@witcom.de>
2021-01-28 11:47:13 +0100

    Kcupgrade (#10)
    * Upgrade Keycloak-Libraries
    
    upgradign to newer keycloak library, using native JWT validation
    Upgrade to newer spring/cloud release
    
    * Clean-up

--- src/test/java/de/witcom/api/WitcomApiGatewayApplicationTests.java
+++ src/test/java/de/witcom/api/WitcomApiGatewayApplicationTests.java
@@ -1,25 +1,9 @@
 package de.witcom.api;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import de.witcom.api.repo.SessionRepository;
-import de.witcom.api.model.Session;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
-import de.witcom.api.model.OIDCCerts;
-import de.witcom.api.model.OIDCKey;
-import org.springframework.test.context.TestPropertySource;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-//@TestPropertySource(locations="classpath:application.properties")
-//@ActiveProfiles("staging")
 public class WitcomApiGatewayApplicationTests {
 
 	@Test
